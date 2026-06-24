b6b07bfeb88ea2218fb1283a6d0bc35649807407
JesterDX <galvezascoy@gmail.com>
2026-06-24 16:28:12 -0500

    Update dashboard.ts

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -2,8 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss',
+  styleUrl: './dashboard.scss'
 })
-export class Dashboard {}
+export class Dashboard {
+
+  stats = {
+    totalAlumnos: 0,
+    matriculasActivas: 0,
+    cuotasAlDia: 0,
+    ingresosMes: 0,
+
+    certificaciones: 0,
+    practicas: 0,
+    usuarios: 0,
+    cursos: 0,
+
+    cuotasVencidas: 0,
+    reservas: 0,
+    certificadosPendientes: 0,
+
+    ingresosTotal: 0,
+    totalPagos: 0,
+    pendientesCobro: 0
+  };
+
+}
