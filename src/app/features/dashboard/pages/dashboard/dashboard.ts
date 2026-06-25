bb3ec708900c00e118b9475f452f4bb4f0dd818c
JesterDX <galvezascoy@gmail.com>
2026-06-24 19:06:25 -0500

    Update dashboard.ts

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,31 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { DashboardService } from './dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
-  standalone: true,
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
 
-  stats = {
-    totalAlumnos: 0,
-    matriculasActivas: 0,
-    cuotasAlDia: 0,
-    ingresosMes: 0,
+  private dashboardService = inject(DashboardService);
 
-    certificaciones: 0,
-    practicas: 0,
-    usuarios: 0,
-    cursos: 0,
+  dashboard: any = {};
 
-    cuotasVencidas: 0,
-    reservas: 0,
-    certificadosPendientes: 0,
+  ngOnInit(): void {
+    this.cargarDashboard();
+  }
 
-    ingresosTotal: 0,
-    totalPagos: 0,
-    pendientesCobro: 0
-  };
-
+  cargarDashboard() {
+    this.dashboardService.getDashboard().subscribe({
+      next: (resp) => {
+        this.dashboard = resp.data;
+      }
+    });
+  }
 }
