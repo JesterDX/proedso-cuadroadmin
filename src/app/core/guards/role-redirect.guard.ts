ed60d25536a33962fd1e48e4f49ef117c314101d
JesterDX <galvezascoy@gmail.com>
2026-06-24 20:41:29 -0500

    Update role-redirect.guard.ts

--- src/app/core/guards/role-redirect.guard.ts
+++ src/app/core/guards/role-redirect.guard.ts
@@ -1,1 +1,21 @@
 
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
+
+export const roleRedirectGuard: CanActivateFn = () => {
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const usuario = authService.usuarioActual();
+  const rol = usuario?.rol?.toUpperCase();
+
+  if (rol === 'ADMINISTRADOR') {
+    router.navigate(['/']);
+  } else {
+    router.navigate(['/alumnos']);
+  }
+
+  return false;
+};
