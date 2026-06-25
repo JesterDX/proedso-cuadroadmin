be618864db30a9a24f9caebe4ddb57beb5348db9
JesterDX <galvezascoy@gmail.com>
2026-06-24 21:04:26 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,7 +25,7 @@ export const routes: Routes = [
       path: '',
       loadComponent: () =>
         import('./features/dashboard/pages/dashboard/dashboard').then(m => m.Dashboard)
-    }
+    },
       {
         path: 'cursos',
         loadComponent: () =>
