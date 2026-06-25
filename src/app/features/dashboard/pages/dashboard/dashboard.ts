01afccf202f8f41b6d22825cbeb970876092e629
JesterDX <galvezascoy@gmail.com>
2026-06-24 19:11:28 -0500

    Update dashboard.ts

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { DashboardService } from './dashboard.service';
+import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
