2a43cefe5e9cdf398abfdee863bb14ab1e24be53
JesterDX <galvezascoy@gmail.com>
2026-06-27 17:03:09 -0500

    Rename and refactor PlanesCursoAdminService
    Refactor PlanesCursoAdminService to PlanesCursoService and update imports.

--- src/app/admin/planes-curso/services/planes-curso-admin.ts
+++ src/app/admin/planes-curso/services/planes-curso-admin.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PlanCurso } from '../models/plan-curso.model';
 
 @Injectable({
-  providedIn:'root'
+  providedIn: 'root'
 })
-export class PlanesCursoAdminService{
+export class PlanesCursoService {
+
+  private http = inject(HttpClient);
+
+  private apiUrl = 'https://proedso-back-wtdl.onrender.com/api/planes-curso';
+
+  listar(): Observable<PlanCurso[]> {
+    return this.http.get<PlanCurso[]>(this.apiUrl);
+  }
+
+  crear(data: any) {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  editar(id: number, data: any) {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
+  }
+
+  eliminar(id: number) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 
 }
