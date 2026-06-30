a8a5ea63491468bcf7a58d3386377da175840955
JesterDX <galvezascoy@gmail.com>
2026-06-30 09:31:25 -0500

    Update tipos-curso.ts

--- src/app/admin/tipos-curso/pages/tipos-curso/tipos-curso.ts
+++ src/app/admin/tipos-curso/pages/tipos-curso/tipos-curso.ts
@@ -6,6 +6,9 @@ import {
 } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import Swal from 'sweetalert2';
 
 import { TiposCursoService } from '../../services/tipos-curso.service';
 import { TipoCurso } from '../../models/tipo-curso.model';
@@ -13,17 +16,29 @@ import { TipoCurso } from '../../models/tipo-curso.model';
 @Component({
   selector: 'app-tipos-curso',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './tipos-curso.html',
   styleUrl: './tipos-curso.scss'
 })
 export class TiposCursoComponent implements OnInit {
 
   private service = inject(TiposCursoService);
-
   private cd = inject(ChangeDetectorRef);
 
   tiposCurso: TipoCurso[] = [];
+
+  mostrarFormulario = false;
+
+  nuevoTipo = {
+    codigo: '',
+    nombre: '',
+    duracion_meses: 1,
+    cantidad_maquinas: 1,
+    activo: true
+  };
 
   ngOnInit(): void {
     this.cargarTipos();
@@ -46,6 +61,48 @@ export class TiposCursoComponent implements OnInit {
         console.error(err);
 
         this.cd.detectChanges();
+
+      }
+
+    });
+
+  }
+
+  guardarTipo(): void {
+
+    this.service.crear(this.nuevoTipo).subscribe({
+
+      next: () => {
+
+        Swal.fire({
+          icon: 'success',
+          title: 'Correcto',
+          text: 'Tipo de curso creado correctamente.'
+        });
+
+        this.mostrarFormulario = false;
+
+        this.nuevoTipo = {
+          codigo: '',
+          nombre: '',
+          duracion_meses: 1,
+          cantidad_maquinas: 1,
+          activo: true
+        };
+
+        this.cargarTipos();
+
+      },
+
+      error: (err) => {
+
+        console.error(err);
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'No se pudo crear el tipo de curso.'
+        });
 
       }
 
