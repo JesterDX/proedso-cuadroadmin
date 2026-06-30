70d26f02f40c706c81016653bf44b11517f16693
JesterDX <galvezascoy@gmail.com>
2026-06-30 09:24:08 -0500

    Update tipos-curso.ts

--- src/app/admin/tipos-curso/pages/tipos-curso/tipos-curso.ts
+++ src/app/admin/tipos-curso/pages/tipos-curso/tipos-curso.ts
@@ -1,4 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  ChangeDetectorRef
+} from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 
 import { TiposCursoService } from '../../services/tipos-curso.service';
@@ -15,25 +21,31 @@ export class TiposCursoComponent implements OnInit {
 
   private service = inject(TiposCursoService);
 
+  private cd = inject(ChangeDetectorRef);
+
   tiposCurso: TipoCurso[] = [];
 
   ngOnInit(): void {
     this.cargarTipos();
   }
 
-  cargarTipos() {
+  cargarTipos(): void {
 
     this.service.listar().subscribe({
 
       next: (resp) => {
 
-        this.tiposCurso = resp.data;
+        this.tiposCurso = resp.data || [];
+
+        this.cd.detectChanges();
 
       },
 
-      error: err => {
+      error: (err) => {
 
         console.error(err);
+
+        this.cd.detectChanges();
 
       }
 
