1339f65d8f108c1e169fb6e3743479824ad8ec3a
JesterDX <galvezascoy@gmail.com>
2026-07-01 14:57:14 -0500

    Update planes-curso-admin.ts

--- src/app/admin/planes-curso/services/planes-curso-admin.ts
+++ src/app/admin/planes-curso/services/planes-curso-admin.ts
@@ -1,6 +1,5 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { map } from 'rxjs';
 import { PlanCurso } from '../models/plan-curso.model';
 
@@ -13,13 +12,11 @@ export class PlanesCursoService {
 
   private apiUrl = 'https://proedso-back-wtdl.onrender.com/api/planes-curso';
 
-listarActivos(): Observable<PlanCurso[]> {
-  return this.http
-    .get<{ ok: boolean; data: PlanCurso[] }>(
-      `${this.apiUrl}/activos`
-    )
-    .pipe(map(res => res.data));
-}
+  listarActivos() {
+    return this.http
+      .get<{ ok: boolean; data: PlanCurso[] }>(`${this.apiUrl}/activos`)
+      .pipe(map(r => r.data));
+  }
 
   crear(data: any) {
     return this.http.post(this.apiUrl, data);
@@ -29,12 +26,13 @@ listarActivos(): Observable<PlanCurso[]> {
     return this.http.put(`${this.apiUrl}/${id}`, data);
   }
 
-cambiarEstado(id:number){
+  cambiarEstado(id: number) {
+    return this.http.patch(`${this.apiUrl}/${id}/estado`, {});
+  }
 
-   return this.http.patch(
-      `${this.apiUrl}/${id}/estado`,
-      {}
-   );
-
-}
+  listarTiposCurso() {
+    return this.http.get<any>(
+      'https://proedso-back-wtdl.onrender.com/api/tipos-curso/activos'
+    );
+  }
 }
