470b2e07375dfa87b138a26bcdb3aae2a7cd677b
JesterDX <galvezascoy@gmail.com>
2026-07-07 11:31:33 -0500

    Update and rename Nuevo Documento de texto.txt to maquinas-admin.service.ts

---
+++ src/app/admin/maquinas/services/maquinas-admin.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Maquina } from '../models/maquina.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MaquinasAdminService {
+
+  private http = inject(HttpClient);
+
+  private apiUrl = 'https://proedso-back-wtdl.onrender.com/api/maquinas';
+
+  listar() {
+    return this.http
+      .get<{ ok: boolean; data: Maquina[] }>(this.apiUrl)
+      .pipe(map(r => r.data));
+  }
+
+  crear(data: any) {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  editar(id: number, data: any) {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
+  }
+
+  cambiarEstado(id: number) {
+    return this.http.patch(`${this.apiUrl}/${id}/estado`, {});
+  }
+
+}
