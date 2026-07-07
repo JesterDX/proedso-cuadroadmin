81cb57d9955870212fe06b7d48df15160a8a607c
JesterDX <galvezascoy@gmail.com>
2026-07-07 13:21:08 -0500

    Update maquinas-admin.service.ts

--- src/app/admin/maquinas/services/maquinas-admin.service.ts
+++ src/app/admin/maquinas/services/maquinas-admin.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
-import { Maquina } from '../models/maquina.model';
+import { Maquina } from '../model/maquina.model';
 
 @Injectable({
   providedIn: 'root'
