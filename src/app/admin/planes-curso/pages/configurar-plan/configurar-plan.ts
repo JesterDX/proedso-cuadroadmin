4beb5b69ce2b1c6be5ed6eeeeb3bbd43abedc3bf
JesterDX <galvezascoy@gmail.com>
2026-07-08 10:42:01 -0500

    Update configurar-plan.ts

--- src/app/admin/planes-curso/pages/configurar-plan/configurar-plan.ts
+++ src/app/admin/planes-curso/pages/configurar-plan/configurar-plan.ts
@@ -1,1 +1,39 @@
 
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-configurar-plan',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './configurar-plan.html',
+  styleUrl: './configurar-plan.scss'
+})
+export class ConfigurarPlanComponent implements OnInit {
+
+
+  private route = inject(ActivatedRoute);
+
+
+  idPlan!: number;
+
+
+  ngOnInit(): void {
+
+    this.idPlan = Number(
+      this.route.snapshot.paramMap.get('id')
+    );
+
+
+    console.log(
+      'Plan seleccionado:',
+      this.idPlan
+    );
+
+  }
+
+}
