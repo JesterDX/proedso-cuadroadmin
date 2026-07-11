588a8b81328f9d4fbec8c4d6b08fe998a6e44592
JesterDX <galvezascoy@gmail.com>
2026-07-11 12:52:50 -0500

    Update planes-curso-admin.ts

--- src/app/admin/planes-curso/services/planes-curso-admin.ts
+++ src/app/admin/planes-curso/services/planes-curso-admin.ts
@@ -1,71 +1,129 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
+
 import { PlanCurso } from '../models/plan-curso.model';
 import { PlanMaquina } from '../models/plan-maquina.model';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class PlanesCursoService {
 
+
   private http = inject(HttpClient);
 
-  private apiUrl = 'https://proedso-back-wtdl.onrender.com/api/planes-curso';
+
+  private apiUrl =
+    'https://proedso-back-wtdl.onrender.com/api/planes-curso';
+
+
 
   listarActivos() {
+
     return this.http
-      .get<{ ok: boolean; data: PlanCurso[] }>(`${this.apiUrl}/activos`)
-      .pipe(map(r => r.data));
+      .get<{ ok:boolean; data:PlanCurso[] }>(
+        `${this.apiUrl}/activos`
+      )
+      .pipe(
+        map(r => r.data)
+      );
+
   }
 
-  crear(data: any) {
-    return this.http.post(this.apiUrl, data);
+
+
+  crear(data:any){
+
+    return this.http.post(
+      this.apiUrl,
+      data
+    );
+
   }
 
-  editar(id: number, data: any) {
-    return this.http.put(`${this.apiUrl}/${id}`, data);
+
+
+  editar(id:number,data:any){
+
+    return this.http.put(
+      `${this.apiUrl}/${id}`,
+      data
+    );
+
   }
 
-  cambiarEstado(id: number) {
-    return this.http.patch(`${this.apiUrl}/${id}/estado`, {});
+
+
+  cambiarEstado(id:number){
+
+    return this.http.patch(
+      `${this.apiUrl}/${id}/estado`,
+      {}
+    );
+
   }
 
-  listarTiposCurso() {
+
+
+  listarTiposCurso(){
+
     return this.http.get<any>(
       'https://proedso-back-wtdl.onrender.com/api/tipos-curso/activos'
     );
+
   }
+
+
+
+
   obtenerPorId(id:number){
 
-  return this.http.get<any>(
-    `${this.apiUrl}/${id}`
+    return this.http.get<any>(
+      `${this.apiUrl}/${id}`
     );
+
   }
 
-  guardarConfiguracion(
- id:number,
- maquinas:any[]
-){
 
- return this.http.put<any>(
- 
- `${this.apiUrl}/planes-curso/${id}/configuracion`,
 
- {
-   maquinas
- }
 
- );
-
-}
   obtenerMaquinas(id:number){
 
-  return this.http.get<{
-    ok:boolean,
-    data:PlanMaquina[]
-  }>(
-    `${this.apiUrl}/${id}/maquinas`
-  );
+    return this.http.get<{
+      ok:boolean,
+      data:PlanMaquina[]
+    }>(
+      `${this.apiUrl}/${id}/maquinas`
+    );
 
-}
+  }
+
+
+
+
+
+  // =====================================
+  // GUARDAR CONFIGURACIÓN DEL PLAN
+  // =====================================
+
+  guardarConfiguracion(
+    id:number,
+    maquinas:PlanMaquina[]
+  ){
+
+    return this.http.put<any>(
+
+      `${this.apiUrl}/${id}/configuracion`,
+
+      {
+        maquinas
+      }
+
+    );
+
+  }
+
+
 }
