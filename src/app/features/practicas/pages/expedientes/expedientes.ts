d908e3dfa97cd52ad4c93da167d01bd6af863eaf
JesterDX <galvezascoy@gmail.com>
2026-07-17 12:10:27 -0500

    Update and rename practocas-home.txt to expedientes.ts

---
+++ src/app/features/practicas/pages/expedientes/expedientes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-expedientes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './expedientes.html',
+  styleUrl: './expedientes.scss'
+})
+export class ExpedientesComponent {}
