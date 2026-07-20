834a123e4f53968720935b1e9719f573fb6a4f5a
JesterDX <galvezascoy@gmail.com>
2026-07-20 00:25:54 -0500

    Create practicas-list.ts

---
+++ src/app/features/practicas/pages/practicas-list/practicas-list.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-nueva-sesion-practica',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  templateUrl: './nueva-sesion.html',
+  styleUrl: './nueva-sesion.scss'
+})
+export class NuevaSesionComponent {
+
+  // TODO:
+  // aquí irá toda la lógica que ya tienes actualmente:
+  // - filtros
+  // - cargarPracticas()
+  // - seleccionarAlumno()
+  // - crearAsignacion()
+  // etc.
+
+}
