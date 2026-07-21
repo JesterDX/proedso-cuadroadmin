24112222e3e3fefb7cc8397e1f4aa3a37b79109d
JesterDX <galvezascoy@gmail.com>
2026-07-21 10:52:56 -0500

    Update cronograma-practicas.ts

--- src/app/features/practicas/pages/cronograma-practicas/cronograma-practicas.ts
+++ src/app/features/practicas/pages/cronograma-practicas/cronograma-practicas.ts
@@ -6,222 +6,389 @@ import {
 
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+
+import {
+  ActivatedRoute,
+  Router
+} from '@angular/router';
+
 import {
   DragDropModule,
   CdkDragDrop,
   moveItemInArray
 } from '@angular/cdk/drag-drop';
 
-import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 import { PracticasService } from '../../services/practicas.service';
+
 import html2pdf from 'html2pdf.js';
+
+
 @Component({
   selector: 'app-cronograma-practicas',
   standalone: true,
-  imports: [
+  imports:[
     CommonModule,
     FormsModule,
     DragDropModule
   ],
-  templateUrl: './cronograma-practicas.html',
-  styleUrl: './cronograma-practicas.scss'
+  templateUrl:'./cronograma-practicas.html',
+  styleUrl:'./cronograma-practicas.scss'
 })
-export class CronogramaPracticasComponent implements OnInit {
+export class CronogramaPracticasComponent 
+implements OnInit {
 
-  private route = inject(ActivatedRoute);
-  private practicasService = inject(PracticasService);
-  private router = inject(Router);
-  sesion: any = null;
 
-  loading = true;
+private route = inject(ActivatedRoute);
 
-  horaInicio = '08:00';
+private router = inject(Router);
 
-  duracionSesion = 30;
+private practicasService = inject(PracticasService);
 
-  ngOnInit(): void {
 
-    const id = Number(
-      this.route.snapshot.paramMap.get('id')
-    );
 
-    this.practicasService
-      .obtenerSesionGrupal(id)
-      .subscribe({
+sesion:any = null;
 
-        next: (resp: any) => {
+loading = true;
 
-          this.sesion = resp.data ?? resp;
 
-          this.generarCronograma();
+horaInicio = '08:00';
 
-          this.loading = false;
 
-        },
+duracionSesion = 30;
 
-        error: (err: any) => {
 
-          console.error(err);
 
-          this.loading = false;
+ngOnInit():void{
 
-        }
 
-      });
+const id = Number(
+ this.route.snapshot.paramMap.get('id')
+);
 
-  }
 
-  generarCronograma(): void {
 
-    if (!this.sesion) return;
+this.practicasService
+.obtenerSesionGrupal(id)
 
-    const [hora, minuto] = this.horaInicio
-      .split(':')
-      .map(Number);
+.subscribe({
 
-    const actual = new Date();
+next:(resp:any)=>{
 
-    actual.setHours(hora);
-    actual.setMinutes(minuto);
-    actual.setSeconds(0);
 
-    this.sesion.detalle.forEach((item: any) => {
+this.sesion = resp.data ?? resp;
 
-      const inicio = new Date(actual);
 
-      actual.setMinutes(
-        actual.getMinutes() +
-        item.sesiones_asignadas * this.duracionSesion
-      );
+if(
+ this.sesion?.detalle
+){
 
-      const fin = new Date(actual);
+ this.generarCronograma();
 
-      item.horaInicio = this.formatearHora(inicio);
+}
 
-      item.horaFin = this.formatearHora(fin);
 
-    });
+this.loading=false;
 
-  }
 
-  formatearHora(fecha: Date): string {
+},
 
-    return fecha.toLocaleTimeString(
-      'es-PE',
-      {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false
-      }
-    );
 
-  }
+error:(err)=>{
 
-  drop(event: CdkDragDrop<any[]>): void {
 
-    moveItemInArray(
-      this.sesion.detalle,
-      event.previousIndex,
-      event.currentIndex
-    );
+console.error(
+ 'Error cargando cronograma',
+ err
+);
 
-    this.generarCronograma();
 
-  }
+this.loading=false;
 
-  guardarCronograma(): void {
 
-  const detalle = this.sesion.detalle.map(
-    (item: any, index: number) => ({
+}
 
-      detalleId: item.detalle_id,
 
-      orden: index + 1,
+});
 
-      horaInicio: item.horaInicio,
 
-      horaFin: item.horaFin
+}
 
-    })
-  );
 
-  this.practicasService
-    .guardarCronograma(
-      this.sesion.id,
-      detalle
-    )
-    .subscribe({
 
-      next: () => {
+
+generarCronograma():void{
+
+
+if(
+ !this.sesion?.detalle
+) return;
+
+
+
+const [hora,minuto] =
+this.horaInicio
+.split(':')
+.map(Number);
+
+
+
+let actual = new Date();
+
+
+actual.setHours(hora);
+
+actual.setMinutes(minuto);
+
+actual.setSeconds(0);
+
+
+
+this.sesion.detalle.forEach(
+(item:any)=>{
+
+
+const inicio =
+new Date(actual);
+
+
+
+actual.setMinutes(
+ actual.getMinutes()
+ +
+ (
+ item.sesiones_asignadas *
+ this.duracionSesion
+ )
+);
+
+
+
+const fin =
+new Date(actual);
+
+
+
+item.horaInicio =
+this.formatearHora(inicio);
+
+
+
+item.horaFin =
+this.formatearHora(fin);
+
+
+
+});
+
+
+}
+
+
+
+
+
+formatearHora(fecha:Date):string{
+
+
+return fecha.toLocaleTimeString(
+'es-PE',
+{
+ hour:'2-digit',
+ minute:'2-digit',
+ hour12:false
+}
+);
+
+
+}
+
+
+
+
+
+drop(
+event:CdkDragDrop<any[]>
+){
+
+
+moveItemInArray(
+
+this.sesion.detalle,
+
+event.previousIndex,
+
+event.currentIndex
+
+);
+
+
+
+this.generarCronograma();
+
+
+}
+
+
+
+
+
+
+guardarCronograma():void{
+
+
+const detalle =
+this.sesion.detalle.map(
+(item:any,index:number)=>({
+
+detalleId:item.detalle_id,
+
+orden:index+1,
+
+horaInicio:item.horaInicio,
+
+horaFin:item.horaFin
+
+})
+);
+
+
+
+this.practicasService
+.guardarCronograma(
+this.sesion.id,
+detalle
+)
+
+.subscribe({
+
+next:()=>{
+
 
 Swal.fire({
-  icon:'success',
-  title:'Cronograma guardado'
+
+icon:'success',
+
+title:'Cronograma guardado'
+
 });
-      },
 
-      error: (err) => {
 
-        console.error(err);
+},
 
-        alert("No se pudo guardar.");
 
-      }
+error:(err)=>{
 
-    });
+
+console.error(err);
+
+
+Swal.fire({
+
+icon:'error',
+
+title:'Error',
+
+text:'No se pudo guardar el cronograma'
+
+});
+
+
+}
+
+
+});
+
 
 }
 
-  generarPDF(): void {
 
-  const elemento = document.getElementById('cronogramaPDF');
 
-  if (!elemento) return;
 
-  const opciones = {
 
-    margin: 0.5,
 
-    filename: `Cronograma_${this.sesion.fecha}.pdf`,
 
-    image: {
-      type: 'jpeg',
-      quality: 1
-    },
+generarPDF():void{
 
-    html2canvas: {
-      scale: 2
-    },
 
-    jsPDF: {
-      unit: 'in',
-      format: 'a4',
-      orientation: 'portrait'
-    }
+const elemento =
+document.getElementById(
+'cronogramaPDF'
+);
 
-  };
 
-  html2pdf()
 
-    .set(opciones)
+if(!elemento)return;
 
-    .from(elemento)
 
-    .save();
+
+const opciones:any={
+
+
+margin:0.5,
+
+
+filename:
+`Cronograma_${this.sesion.fecha}.pdf`,
+
+
+image:{
+type:'jpeg',
+quality:1
+},
+
+
+html2canvas:{
+scale:2
+},
+
+
+jsPDF:{
+unit:'in',
+format:'a4',
+orientation:'portrait'
+}
+
+
+};
+
+
+
+html2pdf()
+
+.set(opciones)
+
+.from(elemento)
+
+.save();
+
 
 }
-  iniciarJornada(): void {
 
-  this.router.navigate([
 
-    '/practicas/jornada',
 
-    this.sesion.id
 
-  ]);
+
+
+// ==================================
+// IR A ASISTENCIA
+// ==================================
+
+iniciarJornada():void{
+
+
+this.router.navigate([
+
+'/practicas',
+
+this.sesion.id
+
+]);
+
 
 }
+
+
 
 }
