3980d5f67638437dae47cf9ab00c47009f819cd6
JesterDX <galvezascoy@gmail.com>
2026-08-03 18:27:56 -0500

    Update homologacion-service.ts

--- src/app/features/homologados/homologado-service/homologacion-service.ts
+++ src/app/features/homologados/homologado-service/homologacion-service.ts
@@ -2,6 +2,9 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
+
+
 import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
