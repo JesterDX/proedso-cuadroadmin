263f8de077f56904151f67827aab7a389cae511e
JesterDX <galvezascoy@gmail.com>
2026-08-03 18:30:17 -0500

    Remove commented-out methods in HomologadosService

--- src/app/features/homologados/homologado-service/homologacion-service.ts
+++ src/app/features/homologados/homologado-service/homologacion-service.ts
@@ -2,10 +2,6 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
-
-
-import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
