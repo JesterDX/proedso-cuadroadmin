88b2064412e898318c87a0b9240f000b80e92f85
JesterDX <galvezascoy@gmail.com>
2026-08-05 18:39:35 -0500

    Update homologados-list.ts

--- src/app/features/homologados/homologados-list/homologados-list.ts
+++ src/app/features/homologados/homologados-list/homologados-list.ts
@@ -1,158 +1,23 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+homologados:any[]=[];
 
-import { HomologadosService } from '../homologado-service/homologacion-service';
-@Component({
-  selector: 'app-homologados-list',
-  standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
-  templateUrl: './homologados-list.html',
-  styleUrls: ['./homologados-list.scss']
-})
-export class HomologadosListComponent implements OnInit {
+busqueda:string='';
 
-  private homologadosService = inject(HomologadosService);
-  private router = inject(Router);
+ngOnInit(){
 
-  loading = false;
+this.cargarHomologados();
 
-  homologados:any[] = [];
+}
 
-  busqueda='';
+cargarHomologados(){
 
-  estadoPago='';
+this.service.listar().subscribe({
 
-  estadoDocumento='';
+next:(resp:any)=>{
 
-  vendedor='';
+this.homologados=resp.data;
 
-  paginaActual=1;
+}
 
-  itemsPorPagina=10;
-
-  ngOnInit(): void {
-
-    this.listar();
-
-  }
-
-  listar(){
-
-    this.loading=true;
-
-    this.homologadosService.listar().subscribe({
-
-      next:(resp:any)=>{
-
-        this.homologados=resp.data ?? [];
-
-        this.loading=false;
-
-      },
-
-      error:()=>{
-
-        this.loading=false;
-
-      }
-
-    });
-
-  }
-
-  get homologadosFiltrados(){
-
-    return this.homologados.filter(h=>{
-
-      const coincideBusqueda=
-
-        !this.busqueda ||
-
-        h.alumno?.toLowerCase().includes(this.busqueda.toLowerCase()) ||
-
-        h.dni?.includes(this.busqueda);
-
-      const coincidePago=
-
-        !this.estadoPago ||
-
-        h.estado_pago===this.estadoPago;
-
-      const coincideDocumento=
-
-        !this.estadoDocumento ||
-
-        h.estado_documento===this.estadoDocumento;
-
-      const coincideVendedor=
-
-        !this.vendedor ||
-
-        h.vendedor===this.vendedor;
-
-      return coincideBusqueda &&
-
-      coincidePago &&
-
-      coincideDocumento &&
-
-      coincideVendedor;
-
-    });
-
-  }
-
-  get homologadosPagina(){
-
-    const inicio=(this.paginaActual-1)*this.itemsPorPagina;
-
-    return this.homologadosFiltrados.slice(
-
-      inicio,
-
-      inicio+this.itemsPorPagina
-
-    );
-
-  }
-
-  get totalPaginas(){
-
-    return Math.ceil(
-
-      this.homologadosFiltrados.length/
-
-      this.itemsPorPagina
-
-    );
-
-  }
-
-  cambiarPagina(p:number){
-
-    if(p<1)return;
-
-    if(p>this.totalPaginas)return;
-
-    this.paginaActual=p;
-
-  }
-
-  nuevo(){
-
-    this.router.navigate(['/homologados/nuevo']);
-
-  }
-
-  editar(id:number){
-
-    this.router.navigate(['/homologados',id]);
-
-  }
+});
 
 }
