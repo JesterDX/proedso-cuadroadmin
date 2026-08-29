f5d10410ffe5d9126841507a3edd03163f77c62d
JesterDX <galvezascoy@gmail.com>
2026-08-28 19:11:21 -0500

    Update alumno.model.ts

--- src/app/features/alumnos/models/alumno.model.ts
+++ src/app/features/alumnos/models/alumno.model.ts
@@ -1,5 +1,6 @@
 export interface Alumno {
   id: number;
+  tipo_documento: string;
   dni: string;
   nombres: string;
   apellidos: string;
@@ -18,6 +19,7 @@ export interface Alumno {
 }
 
 export interface AlumnoPayload {
+  tipo_documento: string;
   dni: string;
   nombres: string;
   apellidos: string;
